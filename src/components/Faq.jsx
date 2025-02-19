547ad4d7f6e68ccc7f90845dbcc35115426dda7b
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2025-02-19 14:30:42 +0500

    Add FAQ

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
 
 function Faq() {
   return (
-    <div>Faq</div>
-  )
+    <div>
+      <section className="container my-[50px]">
+        <h1 className="font-bold text-xl text-start">FAQ</h1>
+
+        <div className="flex gap-6 mt-[15px]">
+          <div className="w-full flex flex-col gap-2">
+            <div className="collapse collapse-plus bg-base-200">
+              <input type="radio" name="my-accordion-3" defaultChecked />
+              <div className="collapse-title text-xl font-medium">
+                Click to open this one and close others
+              </div>
+              <div className="collapse-content">
+                <p>hello</p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-200">
+              <input type="radio" name="my-accordion-3" />
+              <div className="collapse-title text-xl font-medium">
+                Click to open this one and close others
+              </div>
+              <div className="collapse-content">
+                <p>hello</p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-200">
+              <input type="radio" name="my-accordion-3" />
+              <div className="collapse-title text-xl font-medium">
+                Click to open this one and close others
+              </div>
+              <div className="collapse-content">
+                <p>hello</p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex flex-col gap-2">
+            <div className="collapse collapse-plus bg-base-200">
+              <input type="radio" name="my-accordion-3" />
+              <div className="collapse-title text-xl font-medium">
+                Click to open this one and close others
+              </div>
+              <div className="collapse-content">
+                <p>hello</p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-200">
+              <input type="radio" name="my-accordion-3" />
+              <div className="collapse-title text-xl font-medium">
+                Click to open this one and close others
+              </div>
+              <div className="collapse-content">
+                <p>hello</p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-200">
+              <input type="radio" name="my-accordion-3" />
+              <div className="collapse-title text-xl font-medium">
+                Click to open this one and close others
+              </div>
+              <div className="collapse-content">
+                <p>hello</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Faq
+export default Faq;
